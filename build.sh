c7f5733e525856b519ac69be8a62fe693848331e
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2024-07-09 15:16:14 -0500

    Forgot build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if cd flutter; then
+  git pull && cd ..
+else
+  git clone https://github.com/flutter/flutter.git
+fi
+
+ls
+flutter/bin/flutter doctor
+flutter/bin/flutter clean
+flutter/bin/flutter config --enable-web
+flutter/bin/flutter build web --web-renderer canvaskit --release
